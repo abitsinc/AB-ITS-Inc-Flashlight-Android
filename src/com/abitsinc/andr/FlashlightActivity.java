7df748432e6afd96a82fe22915d162fb79b9cea6
abitsinc <abitsincwrk1@gmail.com>
2014-10-12 16:40:29 -0500

    updates to work with newer Android devices, minSdk=9 and maxSdk=19, new icons added to allow publishing on Amazon Appstore

--- src/com/abitsinc/andr/FlashlightActivity.java
+++ src/com/abitsinc/andr/FlashlightActivity.java
@@ -23,75 +23,91 @@ import android.graphics.Color;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class FlashlightActivity extends Activity {
-	
-		//private static final String TAG="Flashlight";
-		private android.hardware.Camera cam;
-		private String oldFlashMode;
-		
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) 
-    {
-    	super.onCreate(savedInstanceState);
-        
-    	//Log.d(TAG + " onCreate", "starting...");
-    	setContentView(R.layout.flashlight);
-    	initCamera();
-    }
-    
-    @Override
-    protected void onResume() {
-    	super.onResume();
-    	
-    	//Log.d(TAG + " onResume", "starting...");
-    	//setContentView(R.layout.flashlight);
-    	initCamera();
-    }
-    
-    @Override
-    protected void onPause() {
-    	super.onPause();
-    	
-    	//Log.d(TAG + " onPause", "onPause");
-    	if (cam!=null) {
-    		Parameters campam=cam.getParameters();
-    		if (oldFlashMode!=null)
-    			campam.setFlashMode(oldFlashMode);
-    		cam.stopPreview();
-    		cam.release();
-    		cam=null;
-    	}
-    }
-    
-    private void initCamera() 
-    {
-    	if (cam!=null) return; // should not happen
-    	TextView txtStatus=(TextView)findViewById(R.id.statusText);
-    	cam=Camera.open();
-    	if (cam!=null) {
-    		Parameters campam=cam.getParameters();
-    		oldFlashMode=campam.getFlashMode();
-    		//Log.d(TAG, "flashMode1 " + oldFlashMode);
-    		if (oldFlashMode!=null) { 
-    			campam.setFlashMode(Parameters.FLASH_MODE_TORCH);
-    			cam.setParameters(campam);
-    			txtStatus.setText(R.string.status_set);
-    			cam.startPreview();
-    		}
-    		else {
-    			//if (cam==null) 
-        	//	Log.d(TAG, "No camera found");
-    			//Log.d(TAG, "flashMode not supported"); 
-    			txtStatus.setText(R.string.status_nld);
-    			TextView txtBkupLight=(TextView)findViewById(R.id.tvBkupLightSrc);
-    			txtBkupLight.setBackgroundColor(Color.WHITE);
-    		}
-    	}
-    	else { 
-    		//Log.d(TAG, "Could not open Camera");
-    	}
-    }
+
+	private static final String TAG = "ABITSIncFlashlight";
+	private android.hardware.Camera cam = null;
+	private String oldFlashMode = null;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Log.d(TAG, "onCreate - starting...");
+		setContentView(R.layout.flashlight);
+		Log.d(TAG, "cam " + (cam == null));
+		initCamera();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		Log.d(TAG + " onResume", "starting...");
+		initCamera();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		Log.d(TAG, "onPause " + (cam != null));
+		if (cam != null) {
+			Parameters campam = cam.getParameters();
+			if (oldFlashMode != null)
+				campam.setFlashMode(oldFlashMode);
+			cam.stopPreview();
+			cam.release();
+			cam = null;
+		}
+	}
+
+	private void initCamera() {
+		if (cam != null) {
+			Log.d(TAG, "cam not null during initCam");
+			return; // should not happen
+		}
+		TextView txtStatus = (TextView) findViewById(R.id.statusText);
+
+		int numCameras = Camera.getNumberOfCameras();
+		if (numCameras <= 0) {
+			Log.d(TAG, "number of cameras " + numCameras);
+			this.useScreenAsFlashlight(txtStatus);
+		} else {
+			cam = Camera.open(0);
+			if (cam != null) {
+				completeCameraSetup(txtStatus);
+			} else {
+				Log.d(TAG, "Could not open Camera");
+				this.useScreenAsFlashlight(txtStatus);
+			}
+		}
+	}
+
+	private void completeCameraSetup(TextView txtStatus) {
+		Parameters campam = cam.getParameters();
+		oldFlashMode = campam.getFlashMode();
+		Log.d(TAG, "flashMode1 " + oldFlashMode);
+		if (oldFlashMode == null)
+			oldFlashMode = Parameters.FLASH_MODE_AUTO;
+		campam.setFlashMode(Parameters.FLASH_MODE_TORCH);
+		cam.setParameters(campam);
+		txtStatus.setText(R.string.status_set);
+		cam.startPreview();
+		String tmpFlashMode = campam.getFlashMode();
+		if (tmpFlashMode == null) {
+			Log.d(TAG, "flashMode not supported");
+			this.useScreenAsFlashlight(txtStatus);
+		}
+	}
+
+	private void useScreenAsFlashlight(TextView txtStatus) {
+		txtStatus.setText(R.string.status_nld);
+		TextView txtBkupLight = (TextView) findViewById(R.id.tvBkupLightSrc);
+		txtBkupLight.setBackgroundColor(Color.WHITE);
+	}
 }
